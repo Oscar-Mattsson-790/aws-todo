b2ea325bdf8075e73f555f54fcb521858a5cd974
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-08-29 13:55:10 +0200

    encrypt fix

--- functions/encryptionHandler.js
+++ functions/encryptionHandler.js
@@ -1,27 +1,33 @@
-const { successResponse, errorResponse } = require("../responses/apiResponses");
+const { sendResponse } = require("../responses/apiResponses");
 const { encrypt, decrypt } = require("../services/encryptionService");
 
 exports.handler = async (event) => {
+  const { method, path } = event.requestContext.http;
+  const body = event.body ? JSON.parse(event.body) : {};
+
+  if (method !== "POST") {
+    return sendResponse(400, { message: "Only POST method is allowed" });
+  }
+
+  if (!body.text) {
+    return sendResponse(400, { message: "Missing 'text' in request body" });
+  }
+
   try {
-    const body = event.body ? JSON.parse(event.body) : {};
-
-    if (!body.text) {
-      return errorResponse(400, "Missing 'text' in request body");
+    if (path === "/encrypt") {
+      return sendResponse(200, {
+        encryptedText: encrypt(body.text),
+      });
     }
 
-    switch (event.path) {
-      case "/encrypt":
-        return successResponse({
-          encryptedText: encrypt(body.text),
-        });
-      case "/decrypt":
-        return successResponse({
-          decryptedText: decrypt(body.text),
-        });
-      default:
-        return errorResponse(400, "Invalid endpoint");
+    if (path === "/decrypt") {
+      return sendResponse(200, {
+        decryptedText: decrypt(body.text),
+      });
     }
+
+    return sendResponse(400, { message: "Invalid endpoint" });
   } catch (error) {
-    return errorResponse(500, error.message);
+    return sendResponse(500, { message: error.message });
   }
 };
--- responses/apiResponses.js
+++ responses/apiResponses.js
@@ -1,19 +1,3 @@
-exports.successResponse = (data) => ({
-  statusCode: 200,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-});
-
-exports.errorResponse = (statusCode, message) => ({
-  statusCode: statusCode || 500,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ message }),
-});
-
 exports.sendResponse = (statusCode, data) => {
   if (statusCode >= 400) {
     return {
